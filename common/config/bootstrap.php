05336d1eac740d3ae6f0b3860030a6b9e5526bc2
satriasmita <satriasmita03@gmail.com>
2022-11-21 09:50:35 +0700

    Bootstrap - Change URL image for slider

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -5,5 +5,5 @@ Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@uploads', dirname(dirname(__DIR__)) . '/uploads'); 
 Yii::setAlias('@browseimage', '/uploads'); 
-Yii::setAlias('@root','http://localhost:8080/dprdwebnew/'); // LINK BERFUNGSI AGAR KETIKA SLIDER DI BUAT, IMAGE DAPAT MUNCUL
+Yii::setAlias('@root','http://localhost:8080/kesra/'); // LINK BERFUNGSI AGAR KETIKA SLIDER DI BUAT, IMAGE DAPAT MUNCUL
 

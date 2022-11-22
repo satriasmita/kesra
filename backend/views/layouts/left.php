9f743728377aaf23a9ced1fe42e63dcf7887ef23
satriasmita <satriasmita03@gmail.com>
2022-11-22 09:44:52 +0700

    Left - Melakukan pengelompokan menu berdasarkan hak akses user

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -26,30 +26,79 @@
         </form>
         <!-- /.search form -->
 
+    <?php if (Yii::$app->user->identity->role_id == 1) : ?>
         <?= dmstr\widgets\Menu::widget(
             [
                 'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
                 'items' => [
+                    
                     ['label' => 'Beranda', 'icon' => 'home', 'url' => ['/site/index']],
-                    ['label' => 'Posting', 'icon' => 'users', 'url' => ['/posting']],       
-                    ['label' => 'Pengaturan Slider', 'icon' => 'desktop', 'url' => ['/slider']],
-                    ['label' => 'Manajemen User', 'icon' => 'users', 'url' => ['/user/admin']],
-                    ['label' => 'Profil User', 'icon' => 'user', 'url' => ['/user/profile']],
-                    ['label' => 'Info Akun', 'icon' => 'lock', 'url' => ['/user/account']],
                     [
-                        'label' => 'Pengaturan Tampilan',
+                        'label' => 'Master',
                         'icon' => 'bars',
                         'url' => '#',
                         "items" => [
-                            ['label' => 'Kategori Posting', 'icon' => 'tv', 'url' => ['/posting-kategori']],
+                            ['label' => 'Juz', 'icon' => 'tv', 'url' => ['/master-juz']],
+                            ['label' => 'Sekolah', 'icon' => 'tv', 'url' => ['/master-sekolah']],
+                            ['label' => 'MDA', 'icon' => 'tv', 'url' => ['/master-mda']],
                         ],
                     ],
-                    ['label' => 'Manajemen Galeri', 'icon' => 'folder-open-o', 'url' => ['/posting']],
-                    ['label' => 'Data Instansi', 'icon' => 'bank', 'url' => ['/data-instansi']],
+
+                    // ['label' => 'Posting', 'icon' => 'users', 'url' => ['/posting']], 
+                    ['label' => 'Data Siswa', 'icon' => 'desktop', 'url' => ['/siswa']],      
+                    ['label' => 'Pengaturan Slider', 'icon' => 'desktop', 'url' => ['/slider']],
+                    // [
+                    //     'label' => 'Pengaturan Tampilan',
+                    //     'icon' => 'bars',
+                    //     'url' => '#',
+                    //     "items" => [
+                    //         ['label' => 'Kategori Posting', 'icon' => 'tv', 'url' => ['/posting-kategori']],
+                    //     ],
+                    // ],
+                    [
+                        'label' => 'User',
+                        'icon' => 'bars',
+                        'url' => '#',
+                        "items" => [
+                            ['label' => 'Manajemen User', 'icon' => 'users', 'url' => ['/user/admin']],
+                            ['label' => 'Profil User', 'icon' => 'user', 'url' => ['/user/profile']],
+                            ['label' => 'Info Akun', 'icon' => 'lock', 'url' => ['/user/account']],
+                        ],
+                    ],
+                    // ['label' => 'Manajemen Galeri', 'icon' => 'folder-open-o', 'url' => ['/posting']],
+                    // ['label' => 'Data Instansi', 'icon' => 'bank', 'url' => ['/data-instansi']],
 
                 ],
             ]
         ) ?>
+
+    <?php elseif (Yii::$app->user->identity->role_id == 2) : ?>
+        <?= dmstr\widgets\Menu::widget(
+            [
+                'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
+                'items' => [
+                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+                    [
+                        'label' => 'Beranda',
+                        'icon' => 'home',
+                        'url' => ['/'],
+                    ],
+                    ['label' => 'Data Pasutri', 'icon' => 'desktop', 'url' => ['/pasutri']],
+                    [
+                        'label' => 'User',
+                        'icon' => 'bars',
+                        'url' => '#',
+                        "items" => [
+                            ['label' => 'Manajemen User', 'icon' => 'users', 'url' => ['/user/admin']],
+                            ['label' => 'Profil User', 'icon' => 'user', 'url' => ['/user/profile']],
+                            ['label' => 'Info Akun', 'icon' => 'lock', 'url' => ['/user/account']],
+                        ],
+                    ],
+              
+                ],
+            ]
+        ) ?>
+        <?php endif; ?>
 
     </section>
 

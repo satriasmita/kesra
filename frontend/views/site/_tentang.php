44657b14a2a087c259e64b92b3c08844a6004c5d
satriasmita <satriasmita03@gmail.com>
2022-11-23 09:30:05 +0700

    Tentang - Perubahan title dan URL

--- frontend/views/site/_tentang.php
+++ frontend/views/site/_tentang.php
@@ -17,9 +17,8 @@ $root_folder = Yii::getAlias('@root');
       <div class="section-content">
         <div class="row">
           <div class="col-md-8 wow fadeInLeft animated">
-            <h2 class="text-uppercase small-line text">SEJARAH <span class="text-theme-color">DPRD KOTA PARIAMAN</span></h2>
-            <h4 class="mt20 mb20 text-theme-color">DPRD Kota Pariaman periode 2014-2019 terdiri atas 9 fraksi, dengan jumlah anggota 106 orang.</h4>
-            <p>Berdasarkan Ordonantie pembentukan sebagaimana termuat dalam Staatsblad 1926 Nomor 366 dan berlaku tanggal 1 Oktober 1926, Gemeente Batavia telah ditunjuk menjadi Stadsgemeente Batavia dan menyelenggarakan pemerintahan daerahnya menurut SGO. Keanggotaan Gemeenteraad Stadsgemeente sama dgn pada masa Gemeente Batavia, demikian pula mengenai jumlahnya. Pada zaman Jepang, sistem pemerintahan daerah pada waktu itu semula tidak terdapat Dewan-Dewan.</p>
+            <h2 class="text-uppercase small-line text">KESRA <span class="text-theme-color">KOTA PARIAMAN</span></h2>
+            <h4 class="mt20 mb20 text-theme-color"></h4>
             <!-- <div class="row">
               <div class="col-sm-4 col-md-4">
                 <div class="service-item"> <i class="fa fa-user-md pb20"></i>
@@ -41,25 +40,7 @@ $root_folder = Yii::getAlias('@root');
               </div>
             </div> -->
           </div>
-          <div class="col-md-4 wow fadeInRight animated">
-            <h3 class="text-uppercase small-line mb20">Agenda <span class="text-theme-color"> Kegiatan</span></h3>
-            <div class="latest-project">
-                
-                <?php 
-                        $age = (new Agenda)->getDetailAgenda(); 
-                        foreach ($age as $row) 
-                    ?>
-              <ul class="hours_wigdet">
-                <li>Waktu : <?= $row->agenda_waktu; ?></li>
-                <li>Pukul : <?= $row->agenda_pukul; ?></li>
-                <li>Tempat : <?= $row->agenda_tempat; ?></li>
-                <li>Detail Acara : <?= $row->agenda_penjelasan; ?></li>
-
-              </ul>
-              
-            <?php  ?> 
-            </div>
-          </div>
+         
         </div>
       </div>
     </div>

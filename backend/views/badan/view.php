22fd0186cf5ec8cf65c605056899094317278719
satriasmita <satriasmita03@gmail.com>
2022-12-13 08:46:58 +0700

    Update coding view badan

--- backend/views/badan/view.php
+++ backend/views/badan/view.php
@@ -19,8 +19,8 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="box-header">
             <h3 class="box-title"><?= Html::encode($this->title) ?></h3>
             <div class="box-tools pull-right">
-                <?= Html::a('<i class="fa fa-fw fa-pencil-square"></i>Perbarui', ['update', 'id' => $model->badan_id], ['class' => 'btn btn-success']) ?>
-                <?= Html::a('<i class="fa fa-fw fa-trash"></i>Hapus', ['delete', 'id' => $model->badan_id], ['class' => 'btn btn-danger','data' => ['confirm' => 'Anda Yakin Ingin Menghapus Data Ini?', 'method' => 'post',],]) ?>
+                <?= Html::a('<i class="fa fa-fw fa-pencil-square"></i>Update', ['update', 'id' => $model->badan_id], ['class' => 'btn btn-success']) ?>
+                <?= Html::a('<i class="fa fa-fw fa-trash"></i>Delete', ['delete', 'id' => $model->badan_id], ['class' => 'btn btn-danger','data' => ['confirm' => 'Anda Yakin Ingin Menghapus Data Ini?', 'method' => 'post',],]) ?>
             </div>
         </div>
         <div class="box-body">

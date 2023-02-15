fbbcab6e06959516fc921da2bcafe32716927789
satriasmita <satriasmita03@gmail.com>
2023-02-15 11:56:18 +0700

    Pembuatan function index pada site controller

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -71,43 +71,66 @@ class SiteController extends Controller
     //     return $this->render('index');
     // }
 
+    // public function actionIndex()
+    // {
+    //     $getSkpd = Setting::find()->where(['nama_setting'=>'SKPD'])->one();
+    //     $kodeSkpd = $getSkpd->nilai_setting;
+
+    //     $getSatker = Setting::find()->where(['nama_setting'=>'Satker'])->one();
+    //     $kodeSatker = $getSatker->nilai_setting;
+
+    //     $getBidang = Setting::find()->where(['nama_setting'=>'Bidang'])->one();
+    //     $kodeBidang = json_decode($getBidang->nilai_setting);
+
+    //     $getAlamat = (new Setting)->find()->where(['nama_setting'=>'Alamat'])->one();
+    //     $alamat = $getAlamat->nilai_setting;
+
+    //     $getEmail = (new Setting)->find()->where(['nama_setting'=>'e-mail'])->one();
+    //     $email = $getEmail->nilai_setting;
+
+    //     $getTanggal = (new Setting)->find()->where(['nama_setting'=>'Tanggal Berdiri'])->one();
+    //     $tanggalberdiri = $getTanggal->nilai_setting;
+
+    //     $getPimpinan = (new Setting)->find()->where(['nama_setting'=>'Pimpinan'])->one();
+    //     $pimpinan = $getPimpinan->nilai_setting;
+
+    //     $getNipPimpinan = (new Setting)->find()->where(['nama_setting'=>'NIP Pimpinan'])->one();
+    //     $nip_pimpinan = $getNipPimpinan->nilai_setting;
+
+    //     return $this->render('index',[
+    //         'kodeSkpd'=>$kodeSkpd,
+    //         'kodeSatker'=>$kodeSatker,
+    //         'kodeBidang'=>$kodeBidang,
+    //         'alamat'=>$alamat,
+    //         'email'=>$email,
+    //         'tanggalberdiri'=>$tanggalberdiri,
+    //         'pimpinan'=>$pimpinan,
+    //         'nip_pimpinan'=>$nip_pimpinan,
+            
+    //     ]);
+    // }
+
+
     public function actionIndex()
     {
-        $getSkpd = Setting::find()->where(['nama_setting'=>'SKPD'])->one();
-        $kodeSkpd = $getSkpd->nilai_setting;
+        $roleName = Yii::$app->user->identity->role->name;
 
-        $getSatker = Setting::find()->where(['nama_setting'=>'Satker'])->one();
-        $kodeSatker = $getSatker->nilai_setting;
+        if ($roleName == 'Admin Tahfidz') {
+            return $this->redirect(['index-etahfidz']);
+        }elseif ($roleName == 'Admin Sipanah') {
+            return $this->redirect(['index-sipanah']);
+        }
+        return $this->render('index');
+    }
 
-        $getBidang = Setting::find()->where(['nama_setting'=>'Bidang'])->one();
-        $kodeBidang = json_decode($getBidang->nilai_setting);
+    public function actionIndexEtahfidz()
+    {
+        return $this->render('indexetahfidz');
+    }
 
-        $getAlamat = (new Setting)->find()->where(['nama_setting'=>'Alamat'])->one();
-        $alamat = $getAlamat->nilai_setting;
-
-        $getEmail = (new Setting)->find()->where(['nama_setting'=>'e-mail'])->one();
-        $email = $getEmail->nilai_setting;
-
-        $getTanggal = (new Setting)->find()->where(['nama_setting'=>'Tanggal Berdiri'])->one();
-        $tanggalberdiri = $getTanggal->nilai_setting;
-
-        $getPimpinan = (new Setting)->find()->where(['nama_setting'=>'Pimpinan'])->one();
-        $pimpinan = $getPimpinan->nilai_setting;
-
-        $getNipPimpinan = (new Setting)->find()->where(['nama_setting'=>'NIP Pimpinan'])->one();
-        $nip_pimpinan = $getNipPimpinan->nilai_setting;
-
-        return $this->render('index',[
-            'kodeSkpd'=>$kodeSkpd,
-            'kodeSatker'=>$kodeSatker,
-            'kodeBidang'=>$kodeBidang,
-            'alamat'=>$alamat,
-            'email'=>$email,
-            'tanggalberdiri'=>$tanggalberdiri,
-            'pimpinan'=>$pimpinan,
-            'nip_pimpinan'=>$nip_pimpinan,
-            
-        ]);
+    public function actionIndexSipanah()
+    {
+        return $this->render('indexsipanah');
     }
 
     /**
